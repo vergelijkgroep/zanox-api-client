844f14e96cc6587fd9dca0da53016fe46a04d971
Geert Wirken <geert@vergelijkgroep.nl>
2016-11-01 12:15:41 +0100

    Pagination working for sales

--- src/ZanoxClient.php
+++ src/ZanoxClient.php
@@ -1,6 +1,7 @@
 <?php
 namespace vergelijkgroep\ZanoxApi;
 
+use DateTime;
 use Httpful\Request;
 use vergelijkgroep\ZanoxApi\Exceptions\ZanoxApiException;
 
@@ -18,7 +19,12 @@ class ZanoxClient {
     /**
      * @var string Endpoint for Zanox API (without trailing slash)
      */
-    protected $endpoint;
+    protected $endpoint = 'https://api.zanox.com/xml/2011-03-01';
+
+    /**
+     * @var int Items per page
+     */
+    protected $itemsPerPage = 50;
 
     /**
      * ZanoxClient constructor.
@@ -28,22 +34,35 @@ class ZanoxClient {
     public function __construct($connectId, $secretKey) {
         $this->connectId = $connectId;
         $this->secretKey = $secretKey;
-        $this->endpoint = 'https://api.zanox.com/xml/2011-03-01';
     }
 
     /**
      * Get all sales for a given date
-     * @param \DateTime $date Date to retrieve sales for
+     * @param DateTime $date Date to retrieve sales for
+     * @param int      $page Start page (defaults to 0)
      * @return array Sales for this date
      */
-    public function getSalesForDate(\DateTime $date) {
+    public function getSalesForDate(DateTime $date, $page = 0) {
         $saleUri = '/reports/sales/date/' . $date->format('Y-m-d');
-        $saleXml = $this->makeRequest($saleUri);
+
+        $saleXml = $this->makeRequest($saleUri, '?items=' . $this->itemsPerPage . '&page=' . $page);
+        echo $saleUri . '?items=' . $this->itemsPerPage . '&page=' . $page . "\n";
 
         $sales = [];
 
-        foreach ($saleXml->saleItems->saleItem as $saleItem) {
-            $sales[] = Sale::createFromXml($saleItem);
+        if (isset($saleXml->saleItems)) {
+            foreach ($saleXml->saleItems->saleItem as $saleItem) {
+                $sales[] = Sale::createFromXml($saleItem);
+            }
+        }
+
+        // Check whether more iterations are needed:
+        $totalItems = (int)$saleXml->total;
+        $currentPageTotal = (int)$saleXml->items + $this->itemsPerPage * $page;
+
+        // Retrieve more items when
+        if ($totalItems > $currentPageTotal) {
+            $sales = array_merge($sales, $this->getSalesForDate($date, $page + 1));
         }
 
         return $sales;
@@ -54,6 +73,7 @@ class ZanoxClient {
      * @param $resource string URI to request (without the base endpoint)
      * @param $query    string Query parameters
      * @return array|object|string
+     * @throws ZanoxApiException
      */
     protected function makeRequest($resource, $query = "") {
         $signature = $this->signRequest('GET', $resource);
